5bad20f99a6a0aaed0fd4e86b0dc6628a10a2401
subaru nara <snara@student.42tokyo.jp>
2021-11-05 01:29:27 +0900

    m00/ex00

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
 
+namespace {
 std::string	&str_upper(std::string &str) {
 	for (size_t i = 0; i < str.length(); ++i) {
-		str[i] = std::toupper(str[i]);
+		str[i] = toupper(str[i]);
 	}
 	return str;
+}
 }
 
 int	main(int ac, char *av[]) {
 	if (ac <= 1) {
-		std::cout << "* |}+_{:>?' FEEDBACK NOISE ~!@#$%^&*()_+ *" <<std::endl;
-		return 0;
-	}
-	for (int i = 1; i < ac; ++i) {
-		std::string str(av[i]);
-		std::cout << str_upper(str);
+		std::cout << "* |}+_{:>?' FEEDBACK NOISE ~!@#$%^&*()_+ *";
+	} else {
+		for (int i = 1; i < ac; ++i) {
+			std::string str(av[i]);
+			std::cout << str_upper(str);
+		}
 	}
 	std::cout << std::endl;
 	return 0;
